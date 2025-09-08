c9c37eb0592304b1ad19fd2c8dd5344b61becfc2
luckysaintmolotov <209353814+luckysaintmolotov@users.noreply.github.com>
2025-09-09 00:25:54 +0200

    feat: add function to generate random choice for computer

--- main.js
+++ main.js
@@ -2,3 +2,16 @@
 function getRandomInt(max){
     return Math.floor(Math.random() * max)
 }
+function getComputerChoice(){
+    let integer = getRandomInt(3);
+    if (integer === 0){
+        return "Rock"
+    }
+    else if (integer === 1){
+        return "Paper"
+    }
+    else {
+        return "Scissor"
+    }
+
+}
