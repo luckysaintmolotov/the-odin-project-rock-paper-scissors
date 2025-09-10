602a85503c8c3900565c69392e1fbf42797447bf
luckysaintmolotov <209353814+luckysaintmolotov@users.noreply.github.com>
2025-09-11 00:42:57 +0200

    feat: add function to play game from ONClick of h1 elements change made to call the playGame function from the console instead of auto init

--- main.js
+++ main.js
@@ -1,46 +1,44 @@
-
-function getRandomInt(max){
-    return Math.floor(Math.random() * max)
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
-function getComputerChoice(){
-    let integer = getRandomInt(3);
-    if (integer === 0){
-        return "Rock"
-    }
-    else if (integer === 1){
-        return "Paper"
-    }
-    else {
-        return "Scissor"
-    }
 
+function getComputerChoice() {
+    let integer = getRandomInt(3);
+    if (integer === 0) {
+        return "rock";
+    } else if (integer === 1) {
+        return "paper";
+    } else {
+        return "scissor";
+    }
 }
 function getHumanChoice(){
     let choice = prompt("Rock, Paper, Scissor?").toLowerCase()
     return choice
     }
-
-function playRound(user_choice, computer_choice){
+function playRound(user_choice, computer_choice) {
     const win_conditions = {
         "rock": "scissor",
         "paper": "rock",
         "scissor": "paper"
-    }
-    
-    if (user_choice === computer_choice)
-    {
-        return "It's a tie!"
-    }
-    else if (win_conditions[user_choice] === computer_choice)
-    {
-        return "You win!"
-    }
-    else 
-    {
-        return "You lose!"
+    };
+
+    if (user_choice === computer_choice) {
+        return "It's a tie!";
+    } else if (win_conditions[user_choice] === computer_choice) {
+        return "You win!";
+    } else {
+        return "You lose!";
     }
 }
 
+function onClickPlay(choice) {
+    let user_choice = choice; // Get the user choice from the parameter
+    let computer_choice = getComputerChoice(); // Get the computer choice
+    let result = playRound(user_choice, computer_choice); // Play the round
+    console.log(`You chose ${user_choice}, computer chose ${computer_choice}. ${result}`); // Log the result
+    document.getElementById("result").innerText = `Result: You chose ${user_choice}, computer chose ${computer_choice}. ${result}`.toUpperCase();
+}
 function playGame(rounds){
     let user_score = 0
     let computer_score = 0
@@ -62,4 +60,7 @@ function playGame(rounds){
 
     }}
 
-playGame(5)
+// Assign click events to the elements
+document.getElementById("rock").onclick = function() { onClickPlay("rock"); };
+document.getElementById("paper").onclick = function() { onClickPlay("paper"); };
+document.getElementById("scissor").onclick = function() { onClickPlay("scissor"); };
